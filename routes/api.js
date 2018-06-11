b088b17c0b813ce194f23e2d3b226413aea3fc17
yasirmym123 <pakfriendship@gmail.com>
2018-06-11 18:56:08 +0500

    Backend Code Token base Authentication

---
+++ routes/api.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require("../models/user");
+const mongoose = require("mongoose");
+const db = "mongodb://yasir:Yasir1@ds241530.mlab.com:41530/angularauth";
+
+mongoose.connect(
+  db,
+  err => {
+    if (err) {
+      console.log("Error!" + err);
+    } else {
+      console.log("Connected to Mongodb");
+    }
+  }
+);
+
+
+function verifyToken(req, res, next){
+
+  if(!req.headers.authorization) {
+    return res.status(401).send('Unauthorized request');
+  }
+    let token = req.headers.authorization.split(' ')[1]
+    if(token === 'null') {
+      return res.status(401).send('Unauthorized request');
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if(!payload) {
+      return res.status(401).send('Unauthorized request');
+
+    }
+    req.userId = payload.subject
+    next()
+  }
+
+
+router.get("/", (req, res) => {
+  res.send("Heloo from Api Route");
+});
+
+router.post("/register", (req, res) => {
+  let userData = req.body;
+  let user = new User(userData);
+  user.save((error, registeredUser) => {
+    if (error) {
+      console.log(error);
+    } else {
+        let payload = { subject: registeredUser._id}
+        let token = jwt.sign(payload,'secretKey')
+      res.status(200).send({token});
+
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  let userData = req.body;
+  User.findOne({ email: userData.email }, (error, user) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (!user) {
+        res.status(401).send("Invalid Email");
+      } else if (user.password !== userData.password) {
+        res.status(401).send("Invalid Password");
+      } else {
+          let payload = { subject: user._id}
+          let token = jwt.sign(payload, 'secretKey')
+        res.status(200).send({token});
+      }
+    }
+  });
+});
+
+router.get("/events", (req, res) => {
+  events = [
+    {
+      _id: "1",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    },
+    {
+      _id: "2",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    },
+    {
+      _id: "3",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    },
+    {
+      _id: "4",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    },
+    {
+      _id: "5",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    },
+    {
+      _id: "6",
+      name: " Auto Expo",
+      description: " BMW FRARI VOALVO TRUCKS",
+      date: "2012-04-23T18:25:43.511Z"
+    }
+  ];
+
+  res.json(events);
+});
+
+router.get("/special", verifyToken, (req, res) => {
+    events = [
+      {
+        _id: "1",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      },
+      {
+        _id: "2",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      },
+      {
+        _id: "3",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      },
+      {
+        _id: "4",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      },
+      {
+        _id: "5",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      },
+      {
+        _id: "6",
+        name: " Auto Expo",
+        description: " BMW FRARI VOALVO TRUCKS",
+        date: "2012-04-23T18:25:43.511Z"
+      }
+    ];
+  
+    res.json(events);
+  });
+module.exports = router;
